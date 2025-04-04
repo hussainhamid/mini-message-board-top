61354a20fde2b4d6166929330f676c4305832068
hussainhamid <143866624+hussainhamid@users.noreply.github.com>
2025-04-04 18:16:54 +0530

    added production database

--- db/client.js
+++ db/client.js
@@ -12,19 +12,35 @@ added TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 
 INSERT INTO messages (username, message)
 VALUES 
-    ('amando', 'Hii there!'),
-    ('charles', 'Hello world!');
+    ('jesse', 'yo, i got the blue'),
+    ('walter', 'did jesse message you?'),
+    ('skyler', 'I am skyler white, yo'),
+    ('hank', 'hehe, i am dea'),
+    ('mike', 'waltuh, waltuh, waltuh.')
 
 `;
 
 async function main() {
   console.log("seeding...");
+
+  //for local db
+
+  // const client = new Client({
+  //   host: process.env.HOST_ENV,
+  //   user: process.env.USER_ENV,
+  //   database: process.env.DATABASE_ENV,
+  //   password: process.env.PASSWORD_ENV,
+  //   port: process.env.DATABASE_PORT_ENV,
+  // });
+
+  //for prod db
+
   const client = new Client({
-    host: process.env.HOST_ENV,
-    user: process.env.USER_ENV,
-    database: process.env.DATABASE_ENV,
-    password: process.env.PASSWORD_ENV,
-    port: process.env.DATABASE_PORT_ENV,
+    connectionString: process.env.DATABASE_URL,
+
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
   await client.connect();
